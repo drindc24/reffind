a2e816989a71fdb5f66b40358c5f645950ebb7b7
Aldrin dela Cruz <adelacruz0930@gmail.com>
2016-05-30 19:05:43 +0800

    Fixed results page.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,13 +2,13 @@ var reffind = angular.module('reffind',['ngResource']);
 
 reffind.controller('SearchCtrl', ['$scope', '$resource', function($scope, $resource) {
     var Flickr = $resource('/search?search_text=:search_text', {search_text: '@search_text'});
-    $scope.results = [];
     $scope.searchText = '';
     $scope.submit = function() {
         if ($scope.searchText) {
+            $scope.results = [];
             Flickr.query({search_text: $scope.searchText}, function(array){
                     for (i=0; i< array.length; i++){
-                        $scope.results.push({url: "www.flickr.com/photos/" + array[i].attributes.owner + "/" + array[i].attributes.id, title: array[i].attributes.title})
+                        $scope.results.push({url: "https://www.flickr.com/photos/" + array[i].attributes.owner + "/" + array[i].attributes.id, title: array[i].attributes.title, static_url: "https://farm" + array[i].attributes.farm + ".staticflickr.com/" + array[i].attributes.server + "/"+ array[i].attributes.id + "_" + array[i].attributes.secret + ".jpg"})
                     }
                 }
             );
